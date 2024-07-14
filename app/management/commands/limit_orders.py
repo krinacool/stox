d5dc98dc24a3bb1223b6467fba1fdf8a83ad790e
Your Name <your.email@example.com>
2024-07-14 22:59:33 +0200

    all server

--- app/management/commands/limit_orders.py
+++ app/management/commands/limit_orders.py
@@ -13,15 +13,19 @@ class Command(BaseCommand):
         while True:
             obj = Order.objects.filter(status='pending').filter(type='Limit')
             for i in obj:
-                if not market_open(i.segment):
-                    return False
-                    # return JsonResponse({'success': False, 'message': 'Market Closed'})
-                price = symbols.objects.filter(instrument_key=i.instrument_key).first().ltp
-                if (
-                    (i.order_type == 'BUY' and int(price) <= int(i.price)) or
-                    (i.order_type == 'SELL' and int(price) >= int(i.price))
-                ):
-                    market_order(i.user,i.symbol,i.instrument_key,00,i.quantity,i.order_type,i.product,i.stoploss,i.target,i.type)
-                    i.delete()
-            time.sleep(1)
+                try:
+                    if not market_open(i.segment):
+                        return False
+                        # return JsonResponse({'success': False, 'message': 'Market Closed'})
+                    price = symbols.objects.filter(instrument_key=i.instrument_key).first().ltp
+                    if (
+                        (i.order_type == 'BUY' and int(price) <= int(i.price)) or
+                        (i.order_type == 'SELL' and int(price) >= int(i.price))
+                    ):
+                        market_order(i.user,i.symbol,i.instrument_key,00,i.quantity,i.order_type,i.product,i.stoploss,i.target,i.type)
+                        i.delete()
+                except:
+                    pass
+            time.sleep(3)
             self.stdout.write(self.style.SUCCESS('Executed Orders Successfully'))
+
