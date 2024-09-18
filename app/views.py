ac78570dccd6ad682b75363fd37bc28331ec9161
atul <atulg0736@gmail.com>
2024-09-18 19:13:23 +0530

    test1

--- app/views.py
+++ app/views.py
@@ -200,9 +200,9 @@ def add_watchlist(request):
                 try:
                     tagob = tags.objects.filter(tag=tag).first()
                     Watchlist.objects.create(user=request.user,symbol=x['symbol'],segment=x['exchange'],tag=tagob)
+                    messages.success(request,"Watchlist Updated ! ")
                 except Exception as e:
-                    messages.success(request,f"Error {e}, Stock cannot be added.")
-            messages.success(request,"Watchlist Updated Successfully")
+                    messages.error(request,f"Error {e}, Stock cannot be added.")
         except Exception as e:
             messages.error(request,f"Some Error Occured {e}")
     return redirect('/watchlist')
