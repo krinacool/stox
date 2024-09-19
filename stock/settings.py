535e889fb9cdacacf26d4e1089f1e876a123e6c6
atul <atulg0736@gmail.com>
2024-09-19 20:23:07 +0530

    LIVE

--- stock/settings.py
+++ stock/settings.py
@@ -173,7 +173,7 @@ SESSION_COOKIE_AGE = 24 * 60 * 60  # 24 hours in seconds
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
 
-PAYU_ENVIRONMENT = 'TEST'  # Use 'Production' in live
+PAYU_ENVIRONMENT = 'LIVE'  # Use 'Production' in live
 
 
 # File paths
