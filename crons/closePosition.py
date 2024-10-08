ce18782d7ab37c5ac51c8341370aec5fe10c281d
Your Name <your.email@example.com>
2024-10-08 16:21:57 +0530

    cron

--- crons/closePosition.py
+++ crons/closePosition.py
@@ -1,4 +1,16 @@
 import requests
+from datetime import datetime
 
+current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+# Write the current time to a time.txt file
+with open('time.txt', 'w') as file:
+    file.write(f"Request made at: {current_time}")
+
+
+# Make the request
 response = requests.get('https://onstock.in/close_position')
 print(response)
+
+with open('res.txt', 'w') as file:
+    file.write(f"Request made at: {response.content}")
